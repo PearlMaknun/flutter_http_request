43f6b555bc1fa7d378ec750404fd814b0cd744f3
Lu'lu' Al-Maknun <almaknunlulu6@gmail.com>
2019-01-15 13:53:46 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http; //menghandle http req
+import 'dart:async';
+import 'dart:convert'; //konversi http respon ke json format
+
+/**
+ * Author: Lu'lu' Al-Maknun
+ * Course: Youtube Video
+ * data from: https://reqres.in/
+ */
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: MyHomePage(title: 'Http Request'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Map data;
+  List userData;
+
+  Future getData() async {
+    http.Response response =
+        await http.get("https://reqres.in/api/users?page=2");
+    data = json.decode(response.body);
+    //debugPrint(response.body);
+    setState(() {
+      userData = data["data"];
+    });
+    debugPrint(userData.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: userData == null ? 0 : userData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(userData[index]["avatar"]),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
